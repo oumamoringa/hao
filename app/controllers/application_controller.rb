1415d955e8bd09521e87e4a8a321ee754de46c2f
oumarichard <richard.ouma@student.moringaschool.com>
2022-11-08 12:28:25 +0300

    Making requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < Sinatra::Base
 
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "HAO_APP" }.to_json
   end
 #get all landlords
   get "/landlords" do
     Landlord.all.to_json
   end
 
+  # #get all properties
+  # get "/propeties" do
+  #   Property.all.to_json
+  # end
+
+  #get all tenants
+  get "/tenants" do
+    Tenant.all.to_json
+  end
 end
