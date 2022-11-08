58acfc9f4cb83e417b8f6e28249d3729f7321262
oumarichard <richard.ouma@student.moringaschool.com>
2022-11-08 13:30:01 +0300

    Adding the post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+      # allow access-control-allow-origin header on all requests
+      before do
+        response.headers["Access-Control-Allow-Origin"] = "*"
+    end
+
+  # enable CORS preflight requests
+    options "*" do
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    end
+
   # Add your routes here
+  #========================
+  #get requests
   get "/" do
     { message: "HAO_APP" }.to_json
   end
@@ -18,5 +30,29 @@ class ApplicationController < Sinatra::Base
   #get all tenants
   get "/tenants" do
     Tenant.all.to_json
+  end
+
+  #post requests(create property and tenant)
+# Creating property
+    post '/property' do
+        property = Property.create(
+            location: params[:location],
+            property_type: params[:property_type],
+            property_name: params[:property_name],
+            property_size: params[:property_size],
+            landlord_id: params[:landlord_id],
+        )
+        property.to_json
+    end
+
+#creating tenant
+    post '/tenant' do
+     tenant = Property.create(
+          name: params[:name],
+          email: params[:email],
+          phone_number: params[:phone_number],
+          property_id: params[:property_id],
+      )
+      tenant.to_json
   end
 end
